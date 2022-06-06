85ab9a3e8f11e5b100dccd544ede2762b1106877
Indrė <90854409+IndrePet@users.noreply.github.com>
2022-06-06 21:07:33 +0300

    task list

---
+++ src/app/components/tasks-list/tasks-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/models/task';
+
+@Component({
+  selector: 'app-tasks-list',
+  templateUrl: './tasks-list.component.html',
+  styleUrls: ['./tasks-list.component.css'],
+})
+export class TasksListComponent implements OnInit {
+  public tasks: Task[] = [];
+  public taskName: string = '';
+  public taskType: string = '';
+
+  constructor() {
+    this.tasks.push(new Task('Išnešti šiukšles', 'Skubu'));
+    this.tasks.push(new Task('Išvalyti kambarius', 'Rutininis'));
+  }
+
+  ngOnInit(): void {}
+
+  public addTask = () => {
+    this.tasks.push(new Task(this.taskName, this.taskType));
+    console.log(this.tasks);
+  };
+}
